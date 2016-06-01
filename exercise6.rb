c2d4d8bdf60b68383dac3fbc46fa8a7e0fc7c096
Andrew Klar <andrewklar@hotmail.com>
2016-06-01 15:05:37 -0400

    added rice, created print method

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,12 @@
+
+def print_list(grocery_list)
+  grocery_list.each do |item|
+    puts "* #{item}"
+  end
+end
+
 grocery_list = ["chicken", "lettuce", "bread", "beer"]
 
-grocery_list.each do |item|
-  puts "* #{item}"
-end
+grocery_list << "rice"
+
+print_list(grocery_list)
