c0c273470c2d308b4e06f6eaeb0d0edbe009fd8e
Andrew Klar <andrewklar@hotmail.com>
2016-06-01 16:28:22 -0400

    added 5% more students to cohorts / deleted cohort2

--- exercise7.rb
+++ exercise7.rb
@@ -9,7 +9,7 @@ students = {
 # 2
 
 students.map do |key, value|
-  puts " #{key}: #{value}"
+  puts " #{key}: #{value} students"
 end
 
 # 3
@@ -19,3 +19,17 @@ students[:cohort4] = 43
 # 4
 
 puts students.keys
+
+# 5
+
+students.each do |cohort_name, num_of_stu|
+  students[cohort_name] = num_of_stu * 105/100
+end
+
+puts students
+
+# 6
+
+students.delete(:cohort2)
+
+puts students
